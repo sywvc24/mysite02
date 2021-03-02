af86ffd66a32e47723676cc7ad450883ec81e117
sywvc24 <75411125+sywvc24@users.noreply.github.com>
2021-03-02 15:21:18 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -125,28 +125,6 @@ iSObservers.forEach(iSObserver => {
 });
 
 
-const touch = 'ontouchstart' in document.documentElement ||
-    navigator.maxTouchPoints > 0 ||
-    navigator.msMaxTouchPoints > 0;
-
-if (touch) { // remove all :hover stylesheets
-    try { // prevent exception on browsers not supporting DOM styleSheets properly
-        for (var si in document.styleSheets) {
-            const styleSheet = document.styleSheets[si];
-            if (!styleSheet.rules) continue;
-
-            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
-                if (!styleSheet.rules[ri].selectorText) continue;
-
-                if (styleSheet.rules[ri].selectorText.match(':hover')) {
-                    styleSheet.deleteRule(ri);
-                }
-            }
-        }
-    } catch (ex) {}
-}
-
-
 const scroll = new SmoothScroll('a[href*="#"]', {
     speedAsDuration: true,
     speed: 1500,
@@ -178,3 +156,25 @@ const tab = new Swiper('.tab-contents', {
         }
     },
 });
+
+
+const touch = 'ontouchstart' in document.documentElement ||
+    navigator.maxTouchPoints > 0 ||
+    navigator.msMaxTouchPoints > 0;
+
+if (touch) { // remove all :hover stylesheets
+    try { // prevent exception on browsers not supporting DOM styleSheets properly
+        for (let si in document.styleSheets) {
+            const styleSheet = document.styleSheets[si];
+            if (!styleSheet.rules) continue;
+
+            for (let ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                if (!styleSheet.rules[ri].selectorText) continue;
+
+                if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                    styleSheet.deleteRule(ri);
+                }
+            }
+        }
+    } catch (ex) {}
+}
